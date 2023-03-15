4f0c4a4b761eb1f29149f43a67ba064fba99408a
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-15 04:18:57 +0300

    Done exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    let result = string
 
+    if (repeat < 0) {
+        result = "ERROR";
+    } else if (repeat == 0) {
+        result = ""
+    } else {
+        for (let i = 1; i < repeat; i++) {
+            result += string;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
