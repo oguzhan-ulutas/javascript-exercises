dab89e6742041226316d12fbf573a9bfc2608ab8
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-15 06:57:45 +0300

    Done exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+   
+    for (remove of toRemove) {
+        let i = array.indexOf(remove);
+        if (i > -1){
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
