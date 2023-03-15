8da14db400928cd5533861e981278f6006d46f00
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-03-15 08:38:50 +0300

    Done exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let sum = 0;
+
+    if (typeof(a) != "number" || typeof(b) != "number") {
+        sum = "ERROR"
+    } else if (a < 0 || b < 0) {
+        sum = "ERROR"
+    } else if (a < b && a >= 0 && b >= 0) {
+        for (a; a <= b; a++) {
+            sum += a;
+        } 
+    } else if (a > b && a >= 0 && b >= 0) {
+        for (b; b <= a; b++) {
+            sum += b
+        }
+    }
+
+    return sum;
 
 };
 
